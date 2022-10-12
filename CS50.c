a8c3260028363180c242f2e782d07010bd764491
TahaShrif <taha14736923@gmail.com>
2022-10-12 04:24:32 +0200

    My First if and else conditions in "C"

--- CS50.c
+++ CS50.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(void)
+int main(void) 
 {
-    printf("What is your name? ");
+    int num1 = 5;
+    int num2 = 6;
+    if(num1 < num2)
+    {
+        printf("num1 is smaller than num2");
+    }
+    else
+    {
+        printf("num1 is not smaller than num2 ");
+    }
 }
